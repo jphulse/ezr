a68aa47c311ef9e74f202dc4098888ca7dcd2da2
jphulse <162621611+jphulse@users.noreply.github.com>
2024-09-12 15:33:09 +0000

    Adding duplicate files to ensure that hw3 functions

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -1,26 +1,45 @@
-import sys,random
+import sys,random, os
 
-from ezr import the, DATA, SYM, COL, NUM, csv, dot, stats
+from ezr import the, DATA, csv
+import stats
+
 
 def guess(N, d) :
     some = random.choices(d.rows, k=N)
     return d.clone().adds(some).chebyshevs().rows
 
+def makeDumb(N, d):
+    dumb = [guess(N,d) for _ in range(20)]
+    dumb = [d.chebyshev(lst[0]) for lst in dumb]
+    return dumb
+
+def makeSmart(d):
+    smart = [d.shuffle().activeLearning() for _ in range(20)]
+    smart = [d.chebyshev(lst[0]) for lst in smart]
+    return smart
+
+def asIs(data):
+    d = DATA().adds(csv(data))
+    b4 = [d.chebyshev(row) for row in d.rows]
+    return [stats.SOME(b4,f"asIs,{len(d.rows)}")]
 
 def experiment(data) :
     somes = []
     for N in [20, 30, 40 , 50]:
-        dNs = stats.SOME(text=f"dumb,{N}")
-        somes += [dNs]
-        sNs = stats.SOME(text=f"smart,{N}")
-        somes += [sNs]
-        d = DATA.new().csv(data)
-        dumb = [guess(N,d) for _ in range(20)]
-        dumb = [d.chebyshev(lst[0]) for lst in dumb]
-        dNs += dumb
+        
         the.last = N
-        smart = [d.shuffle().activeLearning() for _ in range(20)]
-        smart = [d.chebyshev(lst[0]) for lst in smart]
-        sNs += smart 
+        d = DATA().adds(csv(data))
+        dumb = makeDumb(N,d)
+        smart = makeSmart(d)
+        dNs = stats.SOME(dumb, txt=f"dumb,{N}")
+        
+        sNs = stats.SOME(smart, txt=f"smart,{N}")
+
+        somes += [dNs] 
+        somes += [sNs]
+    somes += asIs(data)
+    print(somes)
     stats.report(somes)
 
+print (sys.argv)
+[(experiment(arg)) for arg in sys.argv if arg[-4:] == ".csv"]
